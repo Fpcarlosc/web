c60ee50cc28bcab8237dd860d0bd78a18ca65180
Fpcarlosc <49990214+Fpcarlosc@users.noreply.github.com>
2019-11-03 09:59:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const http = require('http');
+const fs = require('fs');
 const port = process.env.PORT || 3000;
 
 
 var server = http.createServer(function (request, response) {
+         fs.readFile('pizza.html', function(err, data) {
         response.writeHead(200, {"Content-Type": "text/plain"});
-        response.end(utf8.encode("Página oficial de PizzaTime"));
+        response.end(data);
+        });
 });
 
 
