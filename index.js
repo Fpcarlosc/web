116d3dea0414dc739818a14fefc670f1a81fe5ac
Fpcarlosc <49990214+Fpcarlosc@users.noreply.github.com>
2019-11-03 10:02:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const http = require('http');
-const fs = require('fs');
 const port = process.env.PORT || 3000;
 
 
 var server = http.createServer(function (request, response) {
-         fs.readFile('pizza.html', function(err, data) {
         response.writeHead(200, {"Content-Type": "text/plain"});
-        response.end(data);
+        response.end("PizzaTime!);
         });
-});
+
 
 
 server.listen(port);
